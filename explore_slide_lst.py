7b1c8f672909179186d8ffc7fe3b462f76328e1a
ilyahalsky <ik@cone.ee>
2024-04-12 00:19:46 +0300

    sliding

--- explore_slide_lst.py
+++ explore_slide_lst.py
@@ -1,25 +1,78 @@
+import os
+import shutil
+from dataclasses import dataclass
 from itertools import groupby
 from operator import attrgetter as atr
 
 from smn_game import Game
-from smn_logs import read_log_file
+from smn_logs import read_log_file, Minion
 
-def game_to_state(game):
-    opponent = [m.attack_change for m in game.opponents_board]
-    player = [m.attack_change for m in game.players_board]
-    return [*opponent, *player]
 
-def swap(state):
-    swap = state[0]
-    state[0] = state[7]
-    state[7] = swap
-    return state
+@dataclass
+class Ball:
+    attack_change: int
+    minion_id: str
 
-def pipes(before, after):
-    pipe = [0] * 14
-    for i, ball in enumerate(before):
-        pipe[i] = after.index(ball) + 1
-    return pipe
+    @staticmethod
+    def from_minion(minion: Minion):
+        return Ball(minion.attack_change, minion.card_id)
+
+
+@dataclass
+class State:
+    balls: [Ball]
+
+    def __getitem__(self, i):
+        return self.balls[i]
+
+    def index(self, ball: Ball) -> int:
+        return self.balls.index(ball)
+
+    def do_attack(self, player_position: int, opponent_position: int) -> 'State':
+        balls = self.balls.copy()
+        swap = balls[opponent_position - 1]
+        balls[opponent_position - 1] = balls[player_position - 1 + 7]
+        balls[player_position - 1 + 7] = swap
+        return State(balls)
+
+    @staticmethod
+    def from_game(game: Game) -> 'State':
+        opponent = [Ball.from_minion(minion) for minion in game.opponents_board]
+        player = [Ball.from_minion(minion) for minion in game.players_board]
+        return State([*opponent, *player])
+
+    @property
+    def attack_changes(self):
+        return [ball.attack_change for ball in self.balls]
+
+
+@dataclass
+class Pipe:
+    before: State
+    after: State
+
+    def __post_init__(self):
+        self.pipe = [0] * 14
+        for i, ball_before in enumerate(self.before.balls):
+            for j, ball_after in enumerate(self.after.balls):
+                if ball_before.attack_change == ball_after.attack_change:
+                    self.pipe[i] = j
+                    break
+
+    def changed_into(self, ball: Ball):
+        initial_index = self.before.index(ball)
+        next_position = self.pipe[initial_index]
+        return self.after[next_position]
+
+
+def track_minion(minion_id: int, start: State, pipes: [Pipe]):
+    tracking = start.balls[minion_id]
+    trip = [tracking]
+    for pipe in pipes:
+        tracking = pipe.changed_into(tracking)
+        trip.append(tracking)
+    return trip
+
 
 if __name__ == '__main__':
     log_file = "Zone.log"
@@ -41,15 +94,21 @@ if __name__ == '__main__':
             continue
         games.append(game)
 
-    prev_game, *tail = games
-    prev_state = game_to_state(prev_game)
-    prev_state = swap(prev_state)
+    first_game, *tail = games
+    first_state = State.from_game(first_game)
+    prev_state = first_state.do_attack(1, 1)
     all_pipes = []
     for current in tail:
-        current_state = game_to_state(current)
-        pipe = pipes(prev_state, current_state)
+        current_state = State.from_game(current)
+        pipe = Pipe(prev_state, current_state)
         all_pipes.append(pipe)
-        prev_state = swap(current_state)
+        prev_state = current_state.do_attack(1, 1)
 
-
-
+    #for i in range(14):
+    #    trip = track_minion(i, first_state, all_pipes)
+    #    if os.path.exists(f"trip/{i}"):
+    #        shutil.rmtree(f"trip/{i}")
+    #    os.makedirs(f"trip/{i}")
+    #    for j, ball in enumerate(trip):
+    #        print(j, ball)
+    #        shutil.copy(f"image_cache/{ball.minion_id}.png", f"trip/{i}/{j}.png")
