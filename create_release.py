c84edb64f7e166b05fe7f090209df0cf53c6ddd9
ilyahalsky <ik@cone.ee>
2024-04-23 23:02:03 +0300

    multiarch support?

--- create_release.py
+++ create_release.py
@@ -21,7 +21,7 @@ if platform.system() == 'Windows':
     arch = []
 else:
     filename = '-n lst_helper.mac'
-    arch = ['--target-arch universal2']
+    arch = []
 PyInstaller.__main__.run(
     [
         'lst_helper.py',
--- download_card_defs.py
+++ download_card_defs.py
@@ -1,2 +1,13 @@
-import urllib.request
-urllib.request.urlretrieve("http://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json", "cards.collectible.json")
+from tqdm import tqdm
+import requests
+
+def download_file(url):
+    response = requests.get(url, stream=True)
+
+    with open(url.split('/')[-1], "wb") as handle:
+        for data in tqdm(response.iter_content(), desc=url):
+            handle.write(data)
+
+if __name__ == '__main__':
+    download_file("http://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json")
+    download_file("http://api.hearthstonejson.com/v1/latest/enUS/cards.json")
